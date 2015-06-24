36060c4f407b5b524a364a0a9486f05805b584e5
Josh Michielsen <josh.michielsen@gmail.com>
2015-06-25 07:20:30 +0800

    flask-migrate added

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(64))
     about_me = db.Column(db.String(200))
+    is_admin = db.Column(db.Boolean, default=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def is_authenticated(self):
--- db_create.py
+++ db_create.py
@@ -1,13 +1,5 @@
 #!env/bin/python
 
-from migrate.versioning import api
-from config import BaseConfig
 from app import db
-import os.path
 
 db.create_all()
-if not os.path.exists(BaseConfig.SQLALCHEMY_MIGRATE_REPO):
-    api.create(BaseConfig.SQLALCHEMY_MIGRATE_REPO, 'database_repository')
-    api.version_control(BaseConfig.SQLALCHEMY_DATABASE_URI, BaseConfig.SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(BaseConfig.SQLALCHEMY_DATABASE_URI, BaseConfig.SQLALCHEMY_MIGRATE_REPO, api.version(BaseConfig.SQLALCHEMY_MIGRATE_REPO))
